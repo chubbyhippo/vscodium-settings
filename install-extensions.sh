10c49d2747f78774005d1594acefc6c4e7c12164
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-01-25 23:44:23 +0700

    Update install-extensions.sh

--- install-extensions.sh
+++ install-extensions.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env sh
 
+# keybindings
+code --force --install-extension vscodevim.vim
+# sonarqube
+code --force --install-extension SonarSource.sonarlint-vscode
+# git
+code --force --install-extension eamodio.gitlens
+# numbered bookmark
+code --force --install-extension alefragnani.numbered-bookmarks
+# docker
+code --force --install-extension ms-azuretools.vscode-docker
+# yaml
+code --force --install-extension redhat.vscode-yaml
