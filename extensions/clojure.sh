d1b67870c5e8d3c29f3f9a72320c491aab5fad76
chubbyhippo <mkmsacc@outlook.com>
2026-04-23 09:20:41 +0700

    Add script to install Calva extension for Clojure development in VSCodium

---
+++ extensions/clojure.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+RAW_SETTINGS_URL=https://raw.githubusercontent.com/chubbyhippo/vscodium-settings/refs/heads/main
+curl -k "$RAW_SETTINGS_URL/install-default-extensions.sh" | sh
+
+codium --force --install-extension betterthantomorrow.calva
